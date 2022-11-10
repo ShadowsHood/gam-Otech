cca7c1d05b5180fdf5b29751ef4e2a4b8d77f85c
Hellwin <elvinkauffmann@gmail.com>
2022-11-10 11:49:21 +0100

    Le début wesh

--- App.js
+++ App.js
@@ -1,14 +1,52 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+import HomeScreen from './src/screens/HomeScreen';
+
+function DetailsScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'The HOME' }}>
+          {/* A REVOIR PLUS TARD */}
+          {/* {(props) => <HomeScreen {...props} extraData={someData} />} */}
+        </Stack.Screen>
+
+        <Stack.Screen name="Details" component={DetailsScreen} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <View style={styles.container}>
+//         <Text>Open up App.js to start working on your app!</Text>
+//         <StatusBar style="auto" />
+//       </View>
+//     </NavigationContainer>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+
+function HomeScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+            title="Go to Details"
+            onPress={() => navigation.navigate('Details')}
+        />
+        </View>
+    );
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
